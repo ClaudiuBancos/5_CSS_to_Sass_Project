3956cc9c26b26271549457348993b928f132448a
Claudiu Bancos <ClaudiuBancos@gmail.com>
2016-04-25 21:55:45 -0400

    Fixed button

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
 $(".nav-toggle").click(function() {
   $(".main-nav ul").toggle();
 });
+
+var width = $(window).width();
+$(window).resize(function(){
+   if($(this).width() != width){
+    	if ($(this).width() >= 751) {
+    		$(".main-nav ul").show();
+    	} else if (($(this).width() <= 750) && (width >= 751)) {
+    		$(".main-nav ul").hide();
+    	}
+    	width = $(this).width();
+   }
+});
